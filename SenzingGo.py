dfeee48de9fa3a7b53059ed523229c1bbfa95db6
antaenc <ant@senzing.com>
2022-10-31 11:13:29 -0400

    Update SenzingGo.py
    Update version number and date.

--- SenzingGo.py
+++ SenzingGo.py
@@ -30,9 +30,9 @@ except ImportError:
     sys.exit(1)
 
 __all__ = []
-__version__ = '1.6.2'  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = '1.6.3'  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2021-09-10'
-__updated__ = '2022-10-19'
+__updated__ = '2022-10-31'
 
 
 class Colors:
